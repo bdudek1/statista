b67aec6065443f8daf612af1f9b6891e0e0af0e9
bdudek781 <bdudek781@gmail.com>
2020-12-20 21:13:22 +0100

    21.13

--- statista/src/main/java/com/example/statista/repositories/DataSetRepository.java
+++ statista/src/main/java/com/example/statista/repositories/DataSetRepository.java
@@ -4,11 +4,12 @@ import com.example.statista.entities.DataSet;
 import com.example.statista.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface DataSetRepository extends JpaRepository<DataSet, Long> {
 
-    Optional<DataSet> findByUser(User user);
+    List<DataSet> findByUser(User user);
 
     Optional<DataSet> findById(Long id);
 }
